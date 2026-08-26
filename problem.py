cbf2e2f7fb80e01d03e2bd2a28144c6bd33e5bd9
priyaverma25 <priyaknipss25@gmail.com>
2026-08-26 20:55:27 +0530

    day3

--- problem.py
+++ problem.py
@@ -19,53 +19,42 @@
 #        return (high+1)//2 - low//2
    
    
-# how many numbers are smaller than the current number
+# # how many numbers are smaller than the current number
+# class Solution:
+#     def smallerNumbersThanCurrent(self, nums: list[int]) -> list[int]:
+#         result = [] 
+#         for i in nums:
+#             c = 0
+#             for j in nums:
+#                 if j < i:
+#                     c += 1
+#             result.append(c)
+#         return result
+    
+    
+#  Count the Digits With Even Number of Digits
 class Solution:
-    def smallerNumbersThanCurrent(self, nums: list[int]) -> list[int]:
-        result = [] 
-        for i in nums:
-            c = 0
-            for j in nums:
-                if j < i:
-                    c += 1
-            result.append(c)
-        return result
+    def findNumbers(self, nums: list[int]) -> int:
+        count = 0
+        
+        
+        
+        
+
     
-    
-# #  Count the Digits With Even Number of Digits
-# class Solution:
-#     def findNumbers(self, nums: list[int]) -> int:
-#         count = 0
-        
-        
-        
-        
-# # Count the Digits that divide the number
-# class Solution:
-#     def countDigits(self, num: int) -> int:
-#         temp = num
-#         ans = 0
-#         while temp>0:
-#             r =temp%10
-#             if num%r==0:
-#                 ans+=1
-#             temp //= 10
+# subtract the product and sum of digits of an integer
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+        temp = n
+        sum = 0
+        product = 1
+        while temp>0:
+            r = temp % 10
+            product *= r
+            sum += r
+            temp //= 10
             
-#         return ans
-    
-# # subtract the product and sum of digits of an integer
-# class Solution:
-#     def subtractProductAndSum(self, n: int) -> int:
-#         temp = n
-#         sum = 0
-#         product = 1
-#         while temp>0:
-#             r = temp % 10
-#             product *= r
-#             sum += r
-#             temp //= 10
-            
-#         return product - sum
+        return product - sum
 
 
 
