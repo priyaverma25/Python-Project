d92f9889cced91214067dc368ed2de90e567120a
priyaverma25 <priyaknipss25@gmail.com>
2026-09-05 08:48:03 +0530

    day11

--- problem.py
+++ problem.py
@@ -158,20 +158,252 @@
 #         return self.fib(n-1)+self.fib(n-2) 
 
 # # N-th tribonacci number
+# class Solution:
+#     def tribonacci(self, n: int) -> int:
+#         # base case
+#         if n==0:
+#             return 0
+#         if n==1 or n==2:
+#             return 1
+#         # recursive case
+#         return self.tribonacci(n-1)+self.tribonacci(n-2)+self.tribonacci(n-3)
+  
+  
+# Running Sum of 1d Array  
+# class Solution:
+#     def runningSum(self, nums: list[int]) -> list[int]:
+#         n = len(nums) 
+#         ans = []
+#         ans.append(nums[0])
+#         for i in range(1, n):
+#             x = ans[i-1]+nums[i]
+#             ans.append(x)
+#         return ans    
+                   
+                   
+# # Remove Duplicates from Sorted Array (day11)
 class Solution:
-    def tribonacci(self, n: int) -> int:
-        # base case
-        if n==0:
-            return 0
-        if n==1 or n==2:
-            return 1
-        # recursive case
-        return self.tribonacci(n-1)+self.tribonacci(n-2)+self.tribonacci(n-3)
-  
-  
-  
-  
-  
-  
-            
+    def removeDuplicates(self, nums: list[int]) -> int:
+        n = len(nums)
+        start = 0
+        for i in range(1, n):
+            # unique element found
+            if nums[i] != nums[start]:
+                start+=1
+                nums[start] = nums[i]
+                
+        return start+1
+ 
+# Remove Duplicates from Sorted Array II  
+class Solution:
+    def removeDuplicates(self, nums: list[int]) -> int:
+        n = len(nums)
+        
+        if n<=2:
+            return n
+        
+        start = 1
+        for i in range(2, n):
+            # unique element found
+            if nums[i] != nums[start-1]:
+                start+=1
+                nums[start] = nums[i]
+                         
+        return start+1
     
+# # Sort Array By parity
+# class Solution: 
+#     def sortArrayByParity(self, nums: list[int]) -> list[int]:
+#         n = len(nums)
+        
+#         stsrt = 0
+#         for i in range(n):
+#             if nums[i]%2==0:
+#                 temp = nums[i]
+#                 nums[i] = nums[start]
+#                 nums[start] = temp
+#                 start+=1
+#         return nums        
+    
+# # Maximum Subarray
+# class Solution:
+#     def maxSubArray(self, nums: list[int]) -> int:
+#         curr_sum = 0
+#         max_sum = nums[0]
+        
+#         for i in range(len(nums)):
+#             curr_sum += nums[i]
+#             if curr_sum > max_sum:
+#                 max_sum = curr_sum
+#                 if curr_sum < 0:
+#                     curr_sum = 0
+#         return max_sum      
+    
+    
+# #  Best time to buy and sell stock
+# class Solution:
+#     def maxProfit(self, prices: list[int]) -> int:
+#         min_price = prices[0]
+#         profit = 0
+       
+#         for i in range(1, len(prices)):
+#             curr_profit = prices[i] - min_price
+#             if curr_profit > profit:
+#                profit = curr_profit
+#             min_price = min(min_price, prices[i])
+            
+#         return profit        
+    
+# #  Best time to buy and sell stock II
+# class Solution:
+#     def maxProfit(self, prices: list[int]) -> int:
+#         profit = 0
+#         n = len(prices)
+        
+#         for i in range(n-1):
+#             if prices[i+1] > prices[i]:
+#                 profit += prices[i+1] - prices[i]
+#         return profit
+    
+# # Richest customer wealth
+# class Solution:
+#     def maximumWealth(self, accounts: List[List[int]]) -> int:
+#         ans = 0
+#         for account in accounts:
+#             ans = max(ans, sum(account))
+#         return ans
+    
+    
+    
+# # Spiral matrix
+# class Solution:
+#     def spiralOrder(self, matrix: list[list[int]]) -> list[int]:
+#         n = len(matrix)
+#         m = len(matrix[0])
+        
+#         total = n*m
+#         ans = []
+#         c = 0
+        
+#         colstart = 0
+#         rowstart = 0
+#         colend = m-1
+#         rowend = n-1
+        
+#         while c < total:
+#             # rowstart, colstart->colend
+#             for i in range(colstart, colend+1):
+#                 ans.append(matrix[rowstart][i])
+#                 c+=1
+#             rowstart+=1    
+            
+#             if c==total:
+#                 break
+            
+#             # colend, rowstart->rowend
+#             for i in range(rowstart, rowend+1):
+#                 ans.append(matrix[i][colend])
+#                 c+=1
+#             colend-=1
+            
+#             if c==total:
+#                 break
+            
+#             # rowend, colend->colstart
+#             for i in range(colend, colstart-1, -1):
+#                 ans.append(matrix[rowend][i])
+#                 c+=1
+#             rowend-=1
+            
+#             if c==total:
+#                 break
+            
+#             # colstart, rowend->rowstart
+#             for i in range(rowend, rowstart-1, -1):
+#                 ans.append(matrix[i][colstart])
+#                 c+=1
+#             colstart+=1
+            
+#         return ans         
+    
+    
+# # Defanging an IP Address
+# class Solution:
+#     def defangIPaddr(self, address: str) -> str:
+#         ans = ""
+#         for i in address:
+#             if i!=".":
+#                 ans+=i
+#             else:
+#                 ans+="[.]"
+        
+#         return ans            
+    
+#         # return address.replace(".", "[.]")
+    
+# # Valid palindrome
+# class Solution:
+#     def isAlphanumeric(s):
+#         x = ord(s)
+#         if 97<=x<=122 or 65<=x<=90 or 48<=x<=57:
+#             return True
+#         return False
+#     def isPalindrome(self, s: str) -> bool:
+#         s = s.lower()
+        
+#         i = 0
+#         j = len(s)-1
+        
+#         while i<j:
+#             if not self.isAlphanumeric(s[i]):
+#                 i+=1
+#             elif not self.isAlphanumeric(s[j]):
+#                 j-=1    
+#             elif s[i]==s[j]:
+#                 i+=1
+#                 j-=1
+#             else:
+#                 return False    
+#         return True
+    
+# # Reverse String
+# class Solution:
+#     def reverseString(self, s: list[str]) -> None:
+#         """
+#         Do not return anything, modify s in-place instead.
+#         """
+#         i = 0
+#         j = len(s)-1
+        
+#         while i<j:
+#             temp = s[i]
+#             s[i] = s[j]
+#             s[j] = temp
+#             i+=1
+#             j-=1
+        
+#         # s.reverse()
+        
+# # Reverse Words in a String
+# class Solution:
+#     def reverseWords(self, s: str) -> str:
+#         s = s.strip()
+#         s = s.split()
+        
+#         s.reverse()
+#         return " ".join(s)
+    
+# # Length of last Word
+# class Solution:
+#     def lengthOfLastWord(self, s: str) -> int:
+#         s = s.strip()
+#         n = len(s)
+        
+#         i = n-1
+#         while i>=(-1*n) and s[i]!=" ":
+#             i-=1
+            
+#         i+=1
+#         i*=-1
+        
+#         return i  
