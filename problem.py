0d37730b6159bd0f24dc90266377a769b2bb66c6
priyaverma25 <priyaknipss25@gmail.com>
2026-09-13 08:18:58 +0530

    day19

--- problem.py
+++ problem.py
@@ -314,64 +314,64 @@
     
     
 # Spiral matrix
-class Solution:
-    def spiralOrder(self, matrix: list[list[int]]) -> list[int]:
-        n = len(matrix)
-        m = len(matrix[0])
-        
-        total = n*m
-        ans = []
-        c = 0
-        
-        colstart = 0
-        rowstart = 0
-        colend = m-1
-        rowend = n-1
-        
-        while c < total:
-            # rowstart, colstart->colend
-            for i in range(colstart, colend+1):
-                ans.append(matrix[rowstart][i])
-                c+=1
-            rowstart+=1    
-            
-            if c==total:
-                break
-            
-            # colend, rowstart->rowend
-            for i in range(rowstart, rowend+1):
-                ans.append(matrix[i][colend])
-                c+=1
-            colend-=1
-            if c==total:
-                break
-            # rowend, colend->colstart
-            for i in range(colend, colstart-1, -1):
-                ans.append(matrix[rowend][i])
-                c+=1
-            rowend-=1
-            if c==total:
-                break
-            # colstart, rowend->rowstart
-            for i in range(rowend, rowstart-1, -1):
-                ans.append(matrix[i][colstart])
-                c+=1
-            colstart+=1
-        return ans         
-    
-    
-# # Defanging an IP Address
 # class Solution:
-#     def defangIPaddr(self, address: str) -> str:
-#         ans = ""
-#         for i in address:
-#             if i!=".":
-#                 ans+=i
-#             else:
-#                 ans+="[.]"
-#         return ans            
+#     def spiralOrder(self, matrix: list[list[int]]) -> list[int]:
+#         n = len(matrix)
+#         m = len(matrix[0])
+        
+#         total = n*m
+#         ans = []
+#         c = 0
+        
+#         colstart = 0
+#         rowstart = 0
+#         colend = m-1
+#         rowend = n-1
+        
+#         while c < total:
+#             # rowstart, colstart->colend
+#             for i in range(colstart, colend+1):
+#                 ans.append(matrix[rowstart][i])
+#                 c+=1
+#             rowstart+=1    
+            
+#             if c==total:
+#                 break
+            
+#             # colend, rowstart->rowend
+#             for i in range(rowstart, rowend+1):
+#                 ans.append(matrix[i][colend])
+#                 c+=1
+#             colend-=1
+#             if c==total:
+#                 break
+#             # rowend, colend->colstart
+#             for i in range(colend, colstart-1, -1):
+#                 ans.append(matrix[rowend][i])
+#                 c+=1
+#             rowend-=1
+#             if c==total:
+#                 break
+#             # colstart, rowend->rowstart
+#             for i in range(rowend, rowstart-1, -1):
+#                 ans.append(matrix[i][colstart])
+#                 c+=1
+#             colstart+=1
+#         return ans         
     
-#         # return address.replace(".", "[.]")
+    
+# # Defanging an IP Address(day19)
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        ans = ""
+        for i in address:
+            if i!=".":
+                ans+=i
+            else:
+                ans+="[.]"
+        return ans            
+    
+        # return address.replace(".", "[.]")
     
 # # Valid palindrome
 # class Solution:
@@ -441,14 +441,14 @@ class Solution:
 
 # class Soiution:
 #         def twoSum(self, nums: list[int], target: int) -> list[int]:
-#                 n = len(nums) 
-#                 dict = {}
+                # n = len(nums) 
+                # dict = {}
                 
-#                 for i in range(n):
-#                         rem = target - nums[i]
-#                         if rem in dict:
-#                                 return [dict[rem], i]
-#                         dict[nums[i]] = i
+                # for i in range(n):
+                #         rem = target - nums[i]
+                #         if rem in dict:
+                #                 return [dict[rem], i]
+                #         dict[nums[i]] = i
                         
 # # Two Sum II - Inpu array is sorted
 # class Solution:
